9465de50702791fa09b41826fe1c634835d2ad6e
Cash DeLeon <cashadeleon@gmail.com>
2019-04-01 21:43:33 -0500

    asdf

---
+++ Frontend-React/src/utils/api/apiConfig.js
@@ -0,0 +1,12 @@
+export const isDevelopment = true;
+
+export const getJSONroute = (route) => {
+  switch (route) {
+    case '/auth':
+      return '/api/routes/auth.json';
+    case '/signup/customer':
+      return '/api/routes/signupCustomer.json';
+    default:
+      return '';
+  }
+};
