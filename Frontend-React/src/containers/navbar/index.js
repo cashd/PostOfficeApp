069533f885508a18cdb4521b88f0ebb092998d55
Cash DeLeon <cashadeleon@gmail.com>
2019-04-16 21:54:11 -0500

    bug

--- Frontend-React/src/containers/navbar/index.js
+++ Frontend-React/src/containers/navbar/index.js
@@ -21,7 +21,7 @@ class Navigation extends React.Component {
     Cookie.remove('driverID');
     Cookie.remove('truckID');
     Cookie.remove('isManager');
-    push('/')
+    window.location.href = 'www.team9postoffice.ga/';
   };
 
     render() {
